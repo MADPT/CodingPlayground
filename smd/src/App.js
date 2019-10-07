4644fa243b6b116d213d6c0385d4ab3ee194ca9c
MADPT <miguelftangelo@gmail.com>
2019-10-08 00:34:15 +0200

    Add Task 1

--- smd/src/App.js
+++ smd/src/App.js
@@ -1,14 +1,85 @@
 import React from 'react';
 import './App.css';
 
+const API = 'https://sport.api.swisstxt.ch/v1/rankings/8481?lang=de';
+
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>MAD</h1>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            groupData: '',
+            teams: []
+        };
+    }
+
+    componentDidMount() {
+        fetch(API)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    groupData: data.sport.name + ' - ' + data.reference.name,
+                    teams: data.rankingItems
+                });
+
+                this.sortTeam('goalPlus');
+            });
+    }
+
+    sortTeam(filter) {
+        let teams = this.state.teams;
+
+        teams.sort(function(a, b) {
+            return b[filter] - a[filter];
+        });
+
+        this.setState({teams: teams});
+    }
+
+    render() {
+        let { groupData, teams } = this.state;
+
+        return (
+        <div className="App">
+            {teams ? (
+                <table>
+                    <caption>{groupData}</caption>
+                    <thead>
+                        <tr>
+                            <th>Rank</th>
+                            <th>Team</th>
+                            <th>Played</th>
+                            <th>Won</th>
+                            <th>Draws</th>
+                            <th>Lost</th>
+                            <th>For</th>
+                            <th>Against</th>
+                            <th>Diff</th>
+                            <th>Points</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {teams.map(team =>
+                            <tr key={team.competitor.id}>
+                                <td>{team.rank}</td>
+                                <td>{team.competitor.name} </td>
+                                <td>{team.games}</td>
+                                <td>{team.gamesWon}</td>
+                                <td>{team.gamesDraw}</td>
+                                <td>{team.gamesLost}</td>
+                                <td>{team.goalPlus}</td>
+                                <td>{team.goalMinus}</td>
+                                <td>{team.goalDifference}</td>
+                                <td>{team.points}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            ) : (
+                <h1>Data unavailable</h1>
+            )}
+        </div>
+        );
+    }
 }
 
 export default App;
