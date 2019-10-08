8cb0d085b4c9e4345d27d8a079b1a751100ea6bf
MADPT <miguelftangelo@gmail.com>
2019-10-09 01:32:25 +0200

    Fetch json-server data

--- nms/src/App.js
+++ nms/src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import './App.css';
 
+const API = 'http://localhost:4000/products';
+
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: []
+        };
+    }
+
+    componentDidMount() {
+        fetch(API)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({products: data})
+            });
+    }
+
     render() {
+        const recProd = 3;
+        let { products } = this.state;
+
         return (
             <div className="App">
-                <h1>MAD</h1>
+                <h1>Recommend Products</h1>
+                <ul>
+                    {products.slice(0, recProd).map(product =>
+                        <li key={product.id}>
+                            <img src={'/' + product['product-photo']} alt={product.label} />
+                            <p>{product.label}</p>
+                            <p>{product['short-description']}</p>
+                        </li>
+                    )}
+                </ul>
             </div>
         );
     }
