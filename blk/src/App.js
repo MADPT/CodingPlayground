4367c5fc1e96459d3d968b2fe05b77f35ab06ea2
MADPT <miguelftangelo@gmail.com>
2020-04-30 14:26:48 +0200

    Add OffersList component

--- blk/src/App.js
+++ blk/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 
+import OffersList from "./components/offersList";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <OffersList />
     </div>
   );
 }
---
+++ blk/src/components/offersList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OffersList = () => {
+  return <h1>Offers List</h1>;
+};
+
+export default OffersList;
