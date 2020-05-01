1cba8814119918fcb5ba4fd51917f839c3102367
MADPT <miguelftangelo@gmail.com>
2020-05-01 10:06:43 +0200

    Refactor CSS to SCSS

--- blk/src/App.js
+++ blk/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 
 import OffersList from "./components/offersList";
 import OffersContextProvider from "./contexts/offersContext";
